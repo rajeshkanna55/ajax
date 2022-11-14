d05ae134ae9935cd9da603ecb203c18690f2bf24
rajeshkanna55 <rkathiravan889@gmail.com>
2022-11-14 18:21:37 +0530

    changes cod

--- data.js
+++ data.js
@@ -13,12 +13,22 @@ try{
             var cell4=row.insertCell(3);
             var cell5=row.insertCell(4);
             var cell6=row.insertCell(5);
+            var cell7=row.insertCell(6);
+            var cell8=row.insertCell(7);
+            var cell9=row.insertCell(8);
+            var cell10=row.insertCell(9);
+            var cell11=row.insertCell(10);
             cell1.innerHTML=Arr[i].id;
             cell2.innerHTML=Arr[i].name;
             cell3.innerHTML=Arr[i].username;
             cell4.innerHTML=Arr[i].email;
             cell5.innerHTML=Arr[i].address.city;
-            cell6.innerHTML='<button class="btn btn-success" onclick="upd(this)">Update</button>&nbsp&nbsp<button class="btn btn-danger" id="del()">Delete</button>';
+            cell6.innerHTML=Arr[i].phone;
+            cell7.innerHTML=Arr[i].address.street;
+            cell8.innerHTML=Arr[i].website;
+            cell9.innerHTML=Arr[i].company.name;
+            cell10.innerHTML=Arr[i].address.zipcode;
+            cell11.innerHTML='<button class="btn btn-success" onclick="upd(this)">Update</button>&nbsp&nbsp<button class="btn btn-danger" id="del" onclick="del(this)">Delete</button>';
         }
     }
     else{
@@ -34,9 +44,13 @@ catch(err)
     console.log(err);
 }
 
+const del=(k)=>{
+    var value=k.parentNode.parentNode.rowIndex;
+    document.getElementById('data').deleteRow(value);
+} 
 const upd=(r)=>{
     try{
-    var row=r.parentNode.parentNode.rowIndex;
+     row=r.parentNode.parentNode.rowIndex;
     var name=document.getElementById('data').rows[row].cells[2].innerHTML;
     var email=document.getElementById('data').rows[row].cells[3].innerHTML;
     var city=document.getElementById('data').rows[row].cells[4].innerHTML;
@@ -55,4 +69,25 @@ const upd=(r)=>{
     {
         console.log(err);
     }
+ 
+    
 }
+
+const save=()=>{
+    try{
+       var uName=document.getElementById('user').value;
+       var eMail=document.getElementById('emailid').value;
+       var City=document.getElementById('city').value;
+       document.getElementById('data').rows[row].cells[2].innerHTML=uName;
+       document.getElementById('data').rows[row].cells[3].innerHTML=eMail;
+       document.getElementById('data').rows[row].cells[4].innerHTML=City;
+
+    }
+    catch(e){
+        console.log(e);
+    }
+}
+
+   
+    
+    
---
+++ post.js
@@ -0,0 +1,4 @@
+const submit=()=>{
+var cname=document.getElementById('cname').Value;
+console.log(cname);
+}
